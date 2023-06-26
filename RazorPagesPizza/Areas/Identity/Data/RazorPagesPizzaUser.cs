7db7883ceb3e15b94527b701635bcc763bcc092c
GorobchikMaxim <pupezm@gmail.com>
2023-06-26 09:00:45 +0200

    Comm#1

---
+++ RazorPagesPizza/Areas/Identity/Data/RazorPagesPizzaUser.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesPizza.Areas.Identity.Data;
+
+public class RazorPagesPizzaUser : IdentityUser
+{
+    [Required]
+    [MaxLength(100)]
+    public string FirstName { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(100)]
+    public string LastName { get; set; } = string.Empty;
+}
